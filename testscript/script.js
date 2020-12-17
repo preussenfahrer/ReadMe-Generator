eb9caf585cdc0c25ece77285b3dcca6e5b7a0edd
Pere Grinus <gregjleighton@gmail.com>
2020-12-17 09:16:09 -0500

    barebones pseudocode begun for read me categories

---
+++ testscript/script.js
@@ -0,0 +1,7 @@
+// question arrays will go here set as const and let
+
+// first array of questions for README info to be updated by user of app
+
+// array for contributors to project
+
+// array for licenses
