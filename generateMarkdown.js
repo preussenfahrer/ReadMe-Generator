69201b94d1aba31f46e134ebe93e5c145732ce8d
Pere Grinus <gregjleighton@gmail.com>
2020-12-18 17:19:10 -0500

    got README.md preview set up properly and displaying with user input in proper fonts, also included table of contents.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,13 +1,37 @@
 // function to generate markdown for README. This will call the data (see line 3) and then in script.js I will call generateMarkdown. Will also do writetofile in here
 function generateMarkdown(data) {
   return `# ${data.proj_name}
-          # ${data.proj_description}
-          # ${data.proj_install}
-          # ${data.proj_information}
-          # ${data.proj_contribution}
-          # ${data.proj_test_instructions}
-          # ${data.proj_licensing}
-          # ${data.proj_confirm} 
+
+
+     \n ## Table of Contents 
+\n-[Description](#Description)
+\n-[Installation](##Installation)
+\n-[Information](##Information)
+\n-[Contribute](##Contribute)
+\n-[Testing](##Testing)
+\n-[Contribute](##Contribute)
+\n-[Licensing](###Licensing)
+         
+    \n ## Description:
+    \n${data.proj_description}
+
+    \n ## Installation      
+    \n ${data.proj_install}
+
+    \n ## Information
+    \n ${data.proj_information}
+          
+    \n ## Contribute
+    \n ${data.proj_contribution}
+    
+   \n ## Testing
+    \n ${data.proj_test_instructions}
+    
+   \n ## Licensing
+    \n ${data.proj_licensing}
+    
+   \n ## Confirmation
+    \n ${data.proj_confirm} 
   `;
 
 }
--- testscript/README.md
+++ testscript/README.md
@@ -1,9 +1,55 @@
-## Greg's New Project
-          # This a really exciting project about the medieval history of Europe
-          # To install this project, please click on the link below
-          # This project is free for anyone who is interested to increase their knowledge of medieval history
-          # To contribute, please feel free to fork this repository
-          # To test this project, do this
-          # Public Domain
-          # Yes 
+# test
+
+
+     
+ ## Table of Contents 
+
+-[Description](#Description)
+
+-[Installation](##Installation)
+
+-[Information](##Information)
+
+-[Contribute](##Contribute)
+
+-[Testing](##Testing)
+
+-[Contribute](##Contribute)
+
+-[Licensing](###Licensing)
+         
+    
+ ## Description:
+    
+test
+
+    
+ ## Installation      
+    
+ test
+
+    
+ ## Information
+    
+ test
+          
+    
+ ## Contribute
+    
+ test
+    
+   
+ ## Testing
+    
+ test
+    
+   
+ ## Licensing
+    
+ MIT License
+    
+   
+ ## Confirmation
+    
+ yes 
   
